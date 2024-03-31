8de300e46f85a63ae35728f6fbb3991ca70f265e
nana <comfortappiah.ca@gmail.com>
2024-03-31 22:35:15 +0000

    adding styles

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -1,20 +1,35 @@
-
+import "./register.css"
+import netflix from "../assets/2.png"
 
 const RegisterComponent = () => {
   return (
-    <div className="container">
-      <h1>Register Form</h1>
-      <form>
-        <label>Enter Name</label>
-        <input type="text" name="name" placeholder="Enter Full name"/>
-        <label>Enter Email</label>
-        <input type="email" name="email" placeholder="Enter Email"/><label>Enter Password</label>
-        <input type="password" name="password" placeholder="Enter Password"/>
-        <button type="submit">Submit</button>
-      </form>
-
-    </div>
-  )
+		<div className='mother-container'>
+			<img className='img' src={netflix} width='170px' height='100px' />
+			<div className='container'>
+				<h1 className='h1'>Sign In</h1>
+				<form>
+					<input
+						type='email'
+						name='email'
+						placeholder='Email or phone number'
+					/>
+					<br />
+					<input type='password' name='password' placeholder='password' />
+					<br />
+					<button type='submit' className="sign-up">Sign in</button>
+					<h3>OR</h3>
+					<input placeholder="Use a sign in code"/>
+					<a>forgot password</a>
+          <input type="checkbox"/> Remember me
+          <h3>New to Netflix? <a>Sign up now</a></h3>
+					<h5>
+						This page is protected by Google reCAPTCHA to ensure you're not a
+						bot. <span>Learn more.</span>
+					</h5>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default RegisterComponent
